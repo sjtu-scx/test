46275f2cb611715d8bf01e23e8455016c406fbfc
sjtuscx <495249899@qq.com>
2025-01-10 12:43:13 +0800

    两数之和

---
+++ test.py
@@ -0,0 +1,13 @@
+def two_sum(nums, target):
+    num_dict = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_dict:
+            return [num_dict[complement], i]
+        num_dict[num] = i
+    return []
+
+# Example usage:
+nums = [2, 7, 11, 15]
+target = 9
+print(two_sum(nums, target))  # Output: [0, 1]
